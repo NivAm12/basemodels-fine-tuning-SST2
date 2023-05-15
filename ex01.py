83224ea4bdce308420d4cd38185458a0008029c0
Niv Amos <49129250+NivAm12@users.noreply.github.com>
2023-05-16 01:11:19 +0300

    create the script for initial training

--- ex01.py
+++ ex01.py
@@ -1,22 +1,20 @@
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification, Trainer, DataCollatorWithPadding, TrainingArguments
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from evaluate import load
 import torch
-import wandb
 import numpy as np
 
 
-def fine_tune_models(models_names: list, dataset_name: str, train_samples: int, val_samples: int, test_samples: int, preprocess_func,
-                      compute_metrics, num_seeds: int = None):
-    train_args = TrainingArguments(output_dir='outputs', report_to='wandb')
+def fine_tune_models(models_names: list, dataset_name: str, preprocess_func, compute_metrics, train_samples: int = -1, val_samples: int = -1,
+                      test_samples: int = -1, num_seeds: int = None):
+    train_args = TrainingArguments(output_dir='outputs', report_to='wandb', use_mps_device=True)
 
     # run on each seed
     for seed in range(num_seeds):
         torch.manual_seed(seed)
 
         # load datasets
-        dataset = load_data(dataset_name=dataset_name, train_samples=train_samples, val_samples=val_samples, test_samples=test_samples,
-                            seed=seed)
+        dataset = load_data(dataset_name=dataset_name, train_samples=train_samples, val_samples=val_samples, test_samples=test_samples)
         
         # run on the models
         for model_name in models_names:
@@ -30,7 +28,7 @@ def fine_tune_models(models_names: list, dataset_name: str, train_samples: int, 
 
             # fine tune the model
             train_res, trainer = train(model=model, tokenizer=tokenizer, train_dataset=train_dataset, val_dataset=val_dataset, train_args=train_args, 
-                               compute_metrics=compute_metrics)
+                               compute_metrics_fn=compute_metrics)
             
             print(train_res)
             
@@ -63,7 +61,7 @@ def load_model(model_name):
     return model, tokenizer
 
 
-def load_data(dataset_name: str, train_samples: int= -1, val_samples: int = -1, test_samples: int = -1):
+def load_data(dataset_name: str, train_samples: int, val_samples: int, test_samples: int):
     train_dataset = load_dataset(dataset_name, split=f"train[:{train_samples}]")
     val_dataset = load_dataset(dataset_name, split=f"validation[:{val_samples}]")
     test_dataset = load_dataset(dataset_name, split=f"test[:{test_samples}]")
@@ -75,12 +73,12 @@ def load_data(dataset_name: str, train_samples: int= -1, val_samples: int = -1, 
     }
 
 
-def preprocess_func(tokenizer, examples):
+def preprocess_func(examples, tokenizer):
     return tokenizer(examples["sentence"], truncation=True)
 
 
 def preprocess_dataset(dataset, tokenizer, preprocess_fn):
-  tokenized_datasets = dataset.map(preprocess_fn, fn_kwargs={"tokenizer": tokenizer})
+  tokenized_datasets = dataset.map(preprocess_fn, fn_kwargs={"tokenizer": tokenizer}, batched=True, batch_size=None)
   tokenized_datasets = tokenized_datasets.remove_columns(["idx", "sentence"])
   tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
   tokenized_datasets = tokenized_datasets.with_format("torch")
@@ -98,4 +96,12 @@ def compute_metrics_func(eval_pred):
 
 
 if __name__ == '__main__':
+    models = ['bert-base-uncased']
+    dataset = 'sst2'
+    train_samples = 100
+    val_samples = 100
+    num_seeds = 1
+
+    fine_tune_models(models, dataset, train_samples=train_samples, val_samples=val_samples, preprocess_func=preprocess_func,
+                      compute_metrics=compute_metrics_func, num_seeds=num_seeds)
     
