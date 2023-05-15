80118a73a1e5304ab186992b486f68b069ffa2ed
Niv Amos <49129250+NivAm12@users.noreply.github.com>
2023-05-15 23:24:35 +0300

    create the training methods

--- ex01.py
+++ ex01.py
@@ -5,6 +5,37 @@ import torch
 import wandb
 
 
+def train(model, tokenizer, train_dataset, val_dataset, train_args, compute_metrics_fn):
+    data_collator = DataCollatorWithPadding(tokenizer)
+
+    trainer = Trainer(
+        model=model,
+        args=train_args,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        compute_metrics=compute_metrics_fn
+    )
+
+    train_res = trainer.train()
+
+    return {
+        "train_result": train_res,
+        "trainer": trainer
+    }
+    
+
+def load_model(model_name):
+    config = AutoConfig.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, config=config)
+
+    return {
+        "model": model,
+        "tokenizer": tokenizer
+    }
+
+
 def load_data(dataset_name: str, train_samples: int= -1, val_samples: int = -1, test_samples: int = -1, seed: int = None):
     if seed:
         torch.manual_seed(seed)
@@ -20,6 +51,14 @@ def load_data(dataset_name: str, train_samples: int= -1, val_samples: int = -1, 
     }
 
 
+def preprocess_func(tokenizer, examples):
+    return tokenizer(examples["sentence"], truncation=True)
 
-dataset = load_data('sst2')
-print(dataset)
+
+def preprocess_dataset(dataset, tokenizer, preprocess_fn):
+  tokenized_datasets = dataset.map(preprocess_fn, fn_kwargs={"tokenizer": tokenizer})
+  tokenized_datasets = tokenized_datasets.remove_columns(["idx", "sentence"])
+  tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+  tokenized_datasets = tokenized_datasets.with_format("torch")
+
+  return tokenized_datasets
